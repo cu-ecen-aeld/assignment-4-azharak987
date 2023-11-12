d0925d8331526d037cbf690b350c52c3d14ca373
Azhar Ali Khan <azhar987k@gmail.com>
2023-11-12 21:22:38 +0500

    Fixed the Finder app

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
-
-if [ "$#" -ne 2 ]; then
-    echo "Error: Invalid No. of arguments. Please provide filesdir and searchstr."
-    exit 1
-fi
-if [ ! -d "$1" ]; then
-    echo "Error: $1 is not a directory."
+if [ "$#" -lt 2 ]; then
+    echo "Error: Insufficient arguments. Please provide filesdir and searchstr."
     exit 1
 fi
 filesdir="$1"
 searchstr="$2"
+if [ -z "$filesdir" ] || [ ! -d "$filesdir" ]; then
+    echo "Error: '$filesdir' is not a valid directory."
+    exit 1
+fi
 num_files=$(find "$filesdir" -type f | wc -l)
-num_matches=$(grep -r -i -c "$searchstr" "$filesdir")
-echo "The number of files are $num_files and the number of matching lines are $num_matches."
+num_matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
+echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
+exit 0
